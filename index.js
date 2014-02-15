7ca2edfa83d6d6b3cb66cf78e30490b9930a3281
deathcap <deathcap@gmx.co.uk>
2014-02-14 21:08:49 -0800

    Add hurt, heal, min/maxHealth

---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var inherits = require('inherits');
+var EventEmitter = require('events').EventEmitter;
+
+module.exports = function(game, opts) {
+  return new Health(game, opts);
+};
+
+function Health(game, opts) {
+  this.maxHealth = opts.maxHealth || 10;
+  this.minHealth = opts.minHealth || 0;
+  this.startHealth = opts.startHealth || this.maxHealth;
+
+  this.value = this.startHealth;
+}
+
+inherits(Health, EventEmitter);
+
+Health.prototype.hurt = function(amount) {
+  var oldValue = this.value;
+  
+  this.value -= amount;
+  if (this.value < this.minHealth) 
+    this.value = this.minHealth;
+
+  this.emit('health', this.value, oldValue);
+  this.emit('hurt', this.amount);
+
+  if (this.value === this.minHealth) {
+    this.emit('die');
+  }
+};
+
+Health.prototype.heal = function(amount) {
+  var oldValue = this.value;
+
+  this.value += amount;
+  if (this.value > this.maxHealth)
+    this.value = this.maxHealth;
+
+  this.emit('health', this.value, oldValue);
+  this.emit('heal', this.amount);
+};
+
